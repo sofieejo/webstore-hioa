afcb1445117e48c7e859f92f754c70a076c38f8c
Sofie Elisabeth Johnsen <sofie_eli@hotmail.com>
2012-10-24 15:09:00 +0200

    setup databases

---
+++ Webstore/setup.sql
@@ -0,0 +1,52 @@
+CREATE TABLE [dbo].[city] (
+    [zipcode] VARCHAR (4)  NOT NULL,
+    [name]    VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([zipcode] ASC)
+);
+
+CREATE TABLE [dbo].[customer] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [firstname] VARCHAR (50)  NOT NULL,
+    [lastname]  VARCHAR (50)  NOT NULL,
+    [address]   VARCHAR (100) NOT NULL,
+    [zipcode]   VARCHAR (4)   NOT NULL,
+    [email]     VARCHAR (100) NOT NULL,
+    [telephone] VARCHAR (8)   NOT NULL,
+    [password]  VARCHAR (256) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_customer_city] FOREIGN KEY ([zipcode]) REFERENCES [dbo].[city] ([zipcode])
+);
+
+CREATE TABLE [dbo].[category] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [name] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[product] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [name]        VARCHAR (50)  NOT NULL,
+    [price]       DECIMAL (18)  NOT NULL,
+    [description] VARCHAR (MAX) DEFAULT ('no description') NOT NULL,
+    [categoryID]  INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_product_category] FOREIGN KEY ([categoryID]) REFERENCES [dbo].[category] ([Id])
+);
+
+CREATE TABLE [dbo].[order] (
+    [Id]         INT      IDENTITY (1, 1) NOT NULL,
+    [date]       DATETIME NOT NULL,
+    [customerID] INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_order_customer] FOREIGN KEY ([customerID]) REFERENCES [dbo].[customer] ([Id])
+);
+
+CREATE TABLE [dbo].[orderdetail] (
+    [Id]        INT IDENTITY (1, 1) NOT NULL,
+    [productID] INT NOT NULL,
+    [quantity]  INT NOT NULL,
+    [orderID]   INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_orderdetail_product] FOREIGN KEY ([productID]) REFERENCES [dbo].[product] ([Id]),
+    CONSTRAINT [FK_orderdetail_order] FOREIGN KEY ([orderID]) REFERENCES [dbo].[order] ([Id])
+);
