848a6a66bd2836b7251a26d0ec137f1d971bca94
Sofie Elisabeth Johnsen <sofie_eli@hotmail.com>
2012-10-28 13:44:33 +0100

    made showOrder but not working yet, need some distans before I look on it again

---
+++ Webstore/Webstore/Controllers/OrderlineResults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Webstore.Controllers
+{
+    class OrderlineResults
+    {
+        public DateTime date { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; } 
+    }
+}
---
+++ Webstore/Webstore/Models/OrderlineResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webstore
+{
+    public class OrderlineResult
+    {
+        public DateTime date { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; } 
+    }
+}
