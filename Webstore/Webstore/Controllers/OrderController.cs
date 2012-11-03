58b1defe5a474184de023ed6ea7075e065d6b5b1
Kat Aquino <katfaquino@gmail.com>
2012-11-03 22:55:32 +0100

    new shopping cart class

--- Webstore/Webstore/Controllers/OrderController.cs
+++ Webstore/Webstore/Controllers/OrderController.cs
@@ -4,26 +4,19 @@ using System.Linq;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-
+using Webstore.Models;
 
 namespace Webstore.Controllers
 {
     public class OrderController : Controller
     {
         [HttpPost]
-        public JsonResult newOrder(Object[][] somestuff)
+        public JsonResult newOrder(ShoppingCart cart)
         {
             JsonResult result = new JsonResult();
 
-            if (somestuff != null)
-            {
-                for (int i = 0; i < somestuff.Length; i++)
-                {
-                    result.Data += somestuff[i] + " ";
-                }
-           
-            }
-            
+            result.Data += cart;
+                       
             result.Data += "hei";
             return result;
         }
---
+++ Webstore/Webstore/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webstore.Models
+{
+    public class ShoppingCart
+    {
+       public Dictionary<string,string> cart { get; set; }
+    }
+}
